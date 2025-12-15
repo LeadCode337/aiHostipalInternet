69bc7122dc9345b6a430e3c8666cafafd22090fa
LeadCode337 <2765514084@qq.com>
2025-12-15 14:44:12 +0800

    当前最近的一次修改1216

---
+++ RuoYi-Cloud-v3.6.6/ruoyi-modules/ruoyi-hospital/src/main/java/com/ruoyi/hospital/service/IHosJobTitleService.java
@@ -0,0 +1,61 @@
+package com.ruoyi.hospital.service;
+
+import java.util.List;
+import com.ruoyi.hospital.domain.HosJobTitle;
+
+/**
+ * 医生职称Service接口
+ *
+ * @author ruoyi
+ * @date 2025-12-15
+ */
+public interface IHosJobTitleService
+{
+    /**
+     * 查询医生职称
+     *
+     * @param id 医生职称主键
+     * @return 医生职称
+     */
+    public HosJobTitle selectHosJobTitleById(Long id);
+
+    /**
+     * 查询医生职称列表
+     *
+     * @param hosJobTitle 医生职称
+     * @return 医生职称集合
+     */
+    public List<HosJobTitle> selectHosJobTitleList(HosJobTitle hosJobTitle);
+
+    /**
+     * 新增医生职称
+     *
+     * @param hosJobTitle 医生职称
+     * @return 结果
+     */
+    public int insertHosJobTitle(HosJobTitle hosJobTitle);
+
+    /**
+     * 修改医生职称
+     *
+     * @param hosJobTitle 医生职称
+     * @return 结果
+     */
+    public int updateHosJobTitle(HosJobTitle hosJobTitle);
+
+    /**
+     * 批量删除医生职称
+     *
+     * @param ids 需要删除的医生职称主键集合
+     * @return 结果
+     */
+    public int deleteHosJobTitleByIds(Long[] ids);
+
+    /**
+     * 删除医生职称信息
+     *
+     * @param id 医生职称主键
+     * @return 结果
+     */
+    public int deleteHosJobTitleById(Long id);
+}
