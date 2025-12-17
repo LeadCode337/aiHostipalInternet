c464f150ca7aeebca4f9557b30b6103558bffa9c
gui703 <3306211554@qq.com>
2025-12-17 10:43:11 +0800

    Revert "Merge branch 'hjl' of https://github.com/LeadCode337/aiHostipalInternet into gy"
    This reverts commit 5b8ce6441d918aa9b8fd9192f45c0f7b5e07deb1, reversing
    changes made to e641960edba78ea583b7300df6ea231de56f2aca.

--- RuoYi-Cloud-v3.6.6/ruoyi-modules/ruoyi-hospital/src/main/java/com/ruoyi/hospital/controller/PubArticleController.java
+++ RuoYi-Cloud-v3.6.6/ruoyi-modules/ruoyi-hospital/src/main/java/com/ruoyi/hospital/controller/PubArticleController.java
@@ -26,7 +26,7 @@ import com.ruoyi.common.core.web.page.TableDataInfo;
  * 宣传中心Controller
  * 
  * @author ruoyi
- * @date 2025-12-16
+ * @date 2025-12-13
  */
 @RestController
 @RequestMapping("/article")
